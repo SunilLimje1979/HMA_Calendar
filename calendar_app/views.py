f605edb6a33ffba6c8e3a70772e10b2b2c38d5ba
Akash Vibhute <vibhuteakash123@gmail.com>
2024-12-23 19:24:07 +0530

    new app

---
+++ calendar_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# First Page - App Install Prompt
+def index(request):
+    return render(request, 'index.html')
+
+# Second Page - Calendar with Slider
+def calendar_view(request):
+    return render(request, 'calendar.html')
