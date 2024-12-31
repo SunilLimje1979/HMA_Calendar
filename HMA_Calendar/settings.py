0be8c8c63a5765c9543d29b09d5af5b94db895c7
Akash Vibhute <vibhuteakash123@gmail.com>
2024-12-31 11:21:32 +0530

    new domain

--- HMA_Calendar/settings.py
+++ HMA_Calendar/settings.py
@@ -119,17 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-# STATIC_URL = "/HMA_Calendar/static/"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "HMA_Calendar/static",
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = "/static/"
+STATIC_URL = "/HMA_Calendar/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "HMA_Calendar/static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATIC_URL = "/static/"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
