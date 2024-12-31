e7a196bc52faadaede68aaff43f981c9ef5561c7
Akash Vibhute <vibhuteakash123@gmail.com>
2024-12-31 10:38:42 +0530

    new domain

--- HMA_Calendar/settings.py
+++ HMA_Calendar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1)%-31au9a^%g)d&gf@o@vyp38d-7ruaa_686ge=@s9yi_z-8w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","drishtis.app"]
+ALLOWED_HOSTS = ["*","drishtis.app","https://www.hadapsarmedicalassociation.com","13.233.211.102"]
 
 
 # Application definition
@@ -119,17 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = "/HMA_Calendar/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "HMA_Calendar/static",
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# STATIC_URL = "/static/"
+# STATIC_URL = "/HMA_Calendar/static/"
 # STATICFILES_DIRS = [
-#     BASE_DIR / "static",
+#     BASE_DIR / "HMA_Calendar/static",
 # ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
